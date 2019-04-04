cc74ee042f074ad4415b32da80f7faa471344a64
yuse <lyzwhh@qq.com>
2019-04-04 09:41:22 +0800

    可选

--- database/migrations/2019_04_03_132219_create_team_table.php
+++ database/migrations/2019_04_03_132219_create_team_table.php
@@ -15,30 +15,30 @@ class CreateTeamTable extends Migration
     {
         Schema::create('team', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('flag');  //0 是本校现场赛　1 是外校现场赛　2 是网络赛
+            $table->string('flag');  //0 是本校现场赛　1 是外校现场赛　2 是网络赛
             $table->string('school')->default('东秦');
             $table->string('team_name');
 
             $table->string('leader');
-            $table->integer('qq0');
+            $table->string('qq0');
             $table->string('sex0')->nullable();
-            $table->integer('mobile0');
+            $table->string('mobile0');
             $table->string('college0')->nullable();
-            $table->integer('std_num0')->nullable();
+            $table->string('std_num0')->nullable();
 
             $table->string('member1')->nullable();
-            $table->integer('qq1')->nullable();
+            $table->string('qq1')->nullable();
             $table->string('sex1')->nullable();
-            $table->integer('mobile1')->nullable();
+            $table->string('mobile1')->nullable();
             $table->string('college1')->nullable();
-            $table->integer('std_num1')->nullable();
+            $table->string('std_num1')->nullable();
 
             $table->string('member2')->nullable();
-            $table->integer('qq2')->nullable();
+            $table->string('qq2')->nullable();
             $table->string('sex2')->nullable();
-            $table->integer('mobile2')->nullable();
+            $table->string('mobile2')->nullable();
             $table->string('college2')->nullable();
-            $table->integer('std_num2')->nullable();
+            $table->string('std_num2')->nullable();
 
             $table->timestamps();
         });
